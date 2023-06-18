0d5ba49e0fda392b0e631926f855d42f7d8e8f45
klspr <klspr@email.com>
2023-06-18 13:04:23 +0800

    push test

--- src/main/java/com/klspr/GitTest.java
+++ src/main/java/com/klspr/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 }
